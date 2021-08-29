d37950a4b35f44920654c0760e2fa65f98f21588
Huite <Huite@users.noreply.github.com>
2021-08-29 11:54:33 +0000

    deploy: 371dd2a227bd54e115e9bec6a9038622b47b4115

--- _downloads/f989545cc7034bd6c0c40e2091705820/spatial_indexing.py
+++ _downloads/f989545cc7034bd6c0c40e2091705820/spatial_indexing.py
@@ -1,6 +1,7 @@
 """
 Spatial indexing
 ================
+```
 
 The goal of a cell tree is to quickly locate cells of an unstructured mesh.
 Unstructured meshes are challenging in this regard: for a given point, we
@@ -19,7 +20,7 @@ efficiently, without exhaustively checking every cell. The implementation in
 * Locating convex polygons (e.g. cells of another mesh)
 * Locating line segments
 
-This example provides a basic introduction to searching a cell tree for each of
+This example provides an introduction to searching a cell tree for each of
 these.
 
 We'll start by importing the required packages with matplotlib for plotting.
@@ -37,10 +38,17 @@ from matplotlib.collections import LineCollection
 from numba_celltree import CellTree2d, demo
 
 ###############################################################################
-# Let's go easy for a start with a rectangular mesh:
-lower_left = (0.0, 0.0)
-upper_right = (10.0, 10.0)
-vertices, faces = meshzoo.rectangle_quad(lower_left, upper_right, n=10)
+# Let's start with a rectangular mesh:
+nx = ny = 10
+x = y = np.linspace(0.0, 10.0, nx + 1)
+vertices = np.array(np.meshgrid(x, y, indexing="ij")).reshape(2, -1).T
+a = np.add.outer(np.arange(nx), nx * np.arange(ny)) + np.arange(ny)
+faces = np.array([a, a + 1, a + nx + 2, a + nx + 1]).reshape(4, -1).T
+
+# TODO: Replace this one meshzoo R is merged.
+# lower_left = (0.0, 0.0)
+# upper_right = (10.0, 10.0)
+# vertices, faces = meshzoo.rectangle_quad(lower_left, upper_right, n=10)
 
 ###############################################################################
 # Determine the edges of the cells, and plot them.
@@ -55,7 +63,7 @@ demo.plot_edges(node_x, node_y, edges, ax, color="black")
 # ---------------
 #
 # We'll build a cell tree first, then look for some points.
-tree = CellTree2d(vertices, faces)
+tree = CellTree2d(vertices, faces, -1)
 points = np.array(
     [
         [-5.0, 1.0],
@@ -95,7 +103,7 @@ demo.plot_edges(node_x, node_y, edges, ax, color="black")
 # There are certainly no rows or columns to speak of!
 #
 # Let's build a new tree, and look for the same points as before.
-tree = CellTree2d(vertices, faces)
+tree = CellTree2d(vertices, faces, -1)
 i = tree.locate_points(points)
 i = i[i != -1]
 
@@ -169,7 +177,14 @@ demo.plot_boxes(box_coords, ax, color="red", linewidth=3)
 # Locating cells
 # --------------
 #
-# Similarly, we can look for other cells (convex polygons):
+# Similarly, we can look for other cells (convex polygons) and compute the
+# overlap:
+#
+# This returns three arrays of equal length:
+#
+# * the index of the face to locate
+# * the index of the face in the celtree
+# * the area of the intersection
 triangle_vertices = np.array(
     [
         [5.0, 3.0],
@@ -188,7 +203,7 @@ triangles = np.array(
 )
 tri_x, tri_y = triangle_vertices.transpose()
 
-tri_i, cell_i = tree.locate_faces(triangle_vertices, triangles, -1)
+tri_i, cell_i, area = tree.intersect_faces(triangle_vertices, triangles, -1)
 cells_0 = cell_i[tri_i == 0]
 cells_1 = cell_i[tri_i == 1]
 
@@ -203,12 +218,7 @@ demo.plot_edges(
 demo.plot_edges(tri_x, tri_y, demo.edges(triangles, -1), ax, color="red", linewidth=3)
 
 ###############################################################################
-# We can also compute how large the overlap is:
-tri_i, cell_i, area = tree.intersect_faces(triangle_vertices, triangles, -1)
-area
-
-###############################################################################
-# Let's color the faces of the mesh by their proportion of overlap. Because our
+# Let's color the faces of the mesh by their ratio of overlap. Because our
 # mesh is triangular, we can represent the triangles as two collections of
 # vectors (V, U). Then the area is half of the absolute value of the cross
 # product of U and V.
@@ -217,29 +227,106 @@ intersection_vertices = vertices[intersection_faces]
 U = intersection_vertices[:, 1] - intersection_vertices[:, 0]
 V = intersection_vertices[:, 2] - intersection_vertices[:, 0]
 full_area = 0.5 * np.abs(np.cross(V, U))
-proportion = area / full_area
+ratio = area / full_area
 
 fig, ax = plt.subplots()
 colored = ax.tripcolor(
     node_x,
     node_y,
     intersection_faces,
-    proportion,
+    ratio,
 )
 fig.colorbar(colored)
 demo.plot_edges(node_x, node_y, edges, ax, color="black")
 demo.plot_edges(tri_x, tri_y, demo.edges(triangles, -1), ax, color="red", linewidth=3)
 
 ###############################################################################
+# ``CellTree2d`` also provides a method to compute overlaps between boxes and a
+# mesh. This may come in handy to compute overlap with a raster, for example to
+# rasterize a mesh.
+dx = 1.0
+xmin = 0.0
+xmax = 10.0
+
+dy = -1.0
+ymin = 0.0
+ymax = 10.0
+
+y, x = np.meshgrid(
+    np.arange(ymax, ymin + dy, dy),
+    np.arange(xmin, xmax + dx, dx),
+    indexing="ij",
+)
+ny = y.shape[0] - 1
+nx = x.shape[1] - 1
+coords = np.column_stack(
+    [a.ravel() for a in [x[:-1, :-1], x[1:, 1:], y[1:, 1:], y[:-1, :-1]]]
+)
+raster_i, cell_i, raster_overlap = tree.intersect_boxes(coords)
+
+###############################################################################
+# We can construct a weight matrix with these arrays. This weight matrix stores
+# for every raster cell (row) the area of overlap with a triangle (column).
+weight_matrix = np.zeros((ny * nx, len(faces)))
+weight_matrix[raster_i, cell_i] = raster_overlap
+
+fig, ax = plt.subplots()
+colored = ax.imshow(weight_matrix)
+_ = fig.colorbar(colored)
+
+###############################################################################
+# This weight matrix can be used for translating data from one mesh to another.
+# Let's generate some mock elevation data for a valley. Then, we'll compute the
+# area weighted mean for every raster cell.
+def saddle_elevation(x, y):
+    return np.sin(0.6 * x + 2) + np.sin(0.2 * y)
+
+
+# Generate an elevation for every triangle
+centroid_x, centroid_y = vertices[faces].mean(axis=1).transpose()
+face_z = saddle_elevation(centroid_x, centroid_y)
+
+# Compute the weighted mean of the triangles per raster cell
+weighted_elevation = np.dot(weight_matrix, face_z)
+area_sum = np.dot(weight_matrix, np.ones(len(faces)))
+mean_elevation = np.full(ny * nx, np.nan)
+intersects = area_sum > 0
+mean_elevation[intersects] = weighted_elevation[intersects] / area_sum[intersects]
+
+fig = plt.figure(figsize=(10, 4))
+ax0 = fig.add_subplot(1, 2, 1, projection="3d")
+ax0.plot_trisurf(
+    node_x, node_y, faces, saddle_elevation(node_x, node_y), cmap="viridis"
+)
+ax1 = fig.add_subplot(1, 2, 2)
+ax1.imshow(mean_elevation.reshape(ny, nx), extent=(xmin, xmax, ymin, ymax))
+demo.plot_edges(node_x, node_y, edges, ax1, color="white")
+
+###############################################################################
+# Such a weight matrix doesn't apply to just boxes and triangles, but to every
+# case of mapping one mesh to another by intersecting cell areas. Note however
+# that the aggregation above is not very efficient. Most of the entries in the
+# weight matrix are 0; a raster cell only intersects a small number triangles.
+# Such a matrix is much more efficiently stored and processed as a `sparse
+# matrix <https://en.wikipedia.org/wiki/Sparse_matrix>`_ (see also Scipy
+# `sparse <https://docs.scipy.org/doc/scipy/reference/sparse.html>`_). The
+# arrays returned by the ``intersect_`` methods of ``CellTree2d`` form a
+# coordinate list (COO).
+#
+# Such a coordinate list can also be easily used to aggregate values with
+# `Pandas <https://pandas.pydata.org/docs/index.html>`_, as Pandas provides
+# very efficient aggregation in the form of `groupby operations
+# <https://pandas.pydata.org/docs/reference/groupby.html>`_.
+#
 # Locating lines
 # --------------
 #
 # As a final example, we will compute the intersections with two lines (edges).
-# This returns three arrays of equal length:
+# This once again returns three arrays of equal length:
 #
 # * the index of the line
 # * the index of the cell
-# * the location of the intersections
+# * the location of the intersection
 edge_coords = np.array(
     [
         [[0.0, 0.0], [10.0, 10.0]],
