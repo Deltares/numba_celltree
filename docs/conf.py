c3d684b7b5a00665c20a585c4155bd64bdce140d
Huite Bootsma <huitebootsma@gmail.com>
2022-03-03 17:34:44 +0100

    Get rid of _templates, add logo via html_theme_options, do more via CSS

--- docs/conf.py
+++ docs/conf.py
@@ -87,17 +87,27 @@ pygments_style = "sphinx"
 #
 html_theme = "pydata_sphinx_theme"
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_css_files = ["theme-deltares.css"]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    "navbar_align": "content",
+    "icon_links": [
+        {
+            "name": "Deltares",
+            "url": "https://deltares.nl/en/",
+            "icon": "_static/deltares-white.svg",
+            "type": "local",
+        },
+    ],
+}
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
