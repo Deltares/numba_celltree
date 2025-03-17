7e1e6c8a045f69789456175a2b5ede5f964ad10a
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-03-17 16:43:15 +0100

    Pass on faces instead of vertices

--- numba_celltree/celltree.py
+++ numba_celltree/celltree.py
@@ -67,8 +67,8 @@ class CellTree2d(CellTree2dBase):
         counter_clockwise(vertices, faces)
 
         bb_coords = build_face_bboxes(faces, vertices)
-        nodes, bb_indices = initialize(
-            vertices, bb_coords, n_buckets, cells_per_leaf
+        nodes, bb_indices= initialize(
+            faces, bb_coords, n_buckets, cells_per_leaf
         )
         self.vertices = vertices
         self.faces = faces
