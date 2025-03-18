55d8cdae0f32bbdd1c8ab9bb97312c13bb99d1e0
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-03-18 15:55:40 +0100

    Trying some extra stuff

--- examples/check.py
+++ examples/check.py
@@ -26,13 +26,16 @@ def to_vector(a: Point, b: Point) -> Vector:
 
 
 def has_overlap(a: float, b: float, p: float, q: float):
-    return (min(a, b) < max(p, q)) and (max(a, b) > min(p, q))
+    return ((min(a, b) - max(p, q)) < TOLERANCE_ON_EDGE) and (
+        (min(p, q) - max(a, b)) < TOLERANCE_ON_EDGE
+    )
 
 
 def left_of(a: Point, p: Point, U: Vector) -> bool:
     # Whether point a is left of vector U
     # U: p -> q direction vector
-    return U.x * (a.y - p.y) > U.y * (a.x - p.x)
+    # return U.x * (a.y - p.y + TOLERANCE_ON_EDGE) > U.y * (a.x - p.x - TOLERANCE_ON_EDGE)
+    return (U.y * (a.x - p.x) + U.x * (a.y - p.y)) < (4.5 * TOLERANCE_ON_EDGE)
 
 
 def lines_intersect(a: Point, b: Point, p: Point, q: Point) -> bool:
@@ -52,14 +55,19 @@ def lines_intersect(a: Point, b: Point, p: Point, q: Point) -> bool:
     # bounds check
     if (not has_overlap(a.x, b.x, p.x, q.x)) or (not has_overlap(a.y, b.y, p.y, q.y)):
         return False
+
     # Check a and b for separation by U (p -> q)
     # and p and q for separation by V (a -> b)
     if (left_of(a, p, U) != left_of(b, p, U)) and (
-        left_of(p, a, V) == left_of(p, b, V)
+        left_of(p, a, V) != left_of(q, a, V)
     ):
         return True
-    if (abs(cross_product(V, to_vector(a, p))) < TOLERANCE_ON_EDGE) and (
-        abs(cross_product(V, to_vector(a, q))) < TOLERANCE_ON_EDGE
+
+    # Detect collinear case, where segments lie on the same infite line.
+    R = to_vector(a, p)
+    S = to_vector(a, q)
+    if (abs(cross_product(V, R)) < TOLERANCE_ON_EDGE) and (
+        abs(cross_product(V, S) < TOLERANCE_ON_EDGE)
     ):
         return True
 
@@ -70,10 +78,24 @@ def lines_intersect(a: Point, b: Point, p: Point, q: Point) -> bool:
 
 a = Point(0.0, 0.0)
 b = Point(4.0, 4.0)
-# p = Point(-1.0, 1.0)
-# q = Point(1.0, -1.05)
-p = Point(1.0, 1.0)
-q = Point(3.0, 3.0)
+# edge not intersection
+# p = Point(3.0, 2.0)
+# q = Point(3.0, 1.0)
+# edge on edge, orthogonal orientation
+# p = Point(1.0, 3.0)
+# q = Point(3.0, 1.0)
+# Vertex on edge
+p = Point(-1.0, 1.0)
+q = Point(1.0, -1.0)
+# Vertex on vertex
+# p = Point(0.0, 0.0)
+# q = Point(1.0, -1.0)
+# Vertex on vertex, equal orientation
+# p = Point(0.0, 0.0)
+# q = Point(-1.0, -1.0)
+# edge on edge, equal orientation
+# p = Point(1.0, 1.0)
+# q = Point(3.0, 3.0)
 U = to_vector(a, b)
 
 # %%
