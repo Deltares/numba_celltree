75f54be268b1ed777acfb60f7b2324cf3b4fc6da
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-03-21 11:09:13 +0100

    Add test for intersect tree edges

--- tests/test_edgecelltree.py
+++ tests/test_edgecelltree.py
@@ -50,3 +50,25 @@ def test_locate_points():
     np.testing.assert_array_equal(
         tree_edge_indices, np.array([-1, -1, 2], dtype=np.int32)
     )
+
+
+def test_intersect_edges():
+    tree = EdgeCellTree2d(vertices, edges)
+    edge_coords = np.array(
+        [
+            [[1.0, -1.0], [1.0, 1.0]],  # 0 orthogonal
+            [[3.0, 1.0], [-1.0, -1.0]],  # 1 two intersctions
+            [[0.0, -1.0], [0.0, 1.0]],  # 2 on start vertex tree
+            [[-2.0, -1.0], [-3.0, -1.0]],  # no interesect
+        ]
+    )
+    actual_edge, actual_tree_edge, actual_xy = tree.intersect_edges(edge_coords)
+    expected_edge = np.array([0, 1, 1, 2], dtype=np.int32)
+    expected_tree_edge = np.array([1, 1, 2, 0], dtype=np.int32)
+    expected_xy = np.array(
+        [[1.0, 0.0], [1.0, 0.0], [2.0, 0.5], [0.0, 0.0]], dtype=float
+    )
+
+    np.testing.assert_array_equal(actual_edge, expected_edge)
+    np.testing.assert_array_equal(actual_tree_edge, expected_tree_edge)
+    np.testing.assert_allclose(actual_xy, expected_xy, atol=TOLERANCE_ON_EDGE)
