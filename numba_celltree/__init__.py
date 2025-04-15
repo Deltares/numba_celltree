a9607c21b604c5a412d6bbcfef7746508405dc6a
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-04-15 16:42:51 +0200

    update version number

--- numba_celltree/__init__.py
+++ numba_celltree/__init__.py
@@ -1,6 +1,6 @@
 from numba_celltree.celltree import CellTree2d
 from numba_celltree.edge_celltree import EdgeCellTree2d
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 __all__ = ("CellTree2d", "EdgeCellTree2d")
